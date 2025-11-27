14aa9b6dd3694cf8980e923b7fd00b871e765689
MarkAaronEstanoco <MarkAaronEstanoco@SMU-0032>
2025-11-27 17:00:00 +0800

    Add project files.

---
+++ Estanoco.MA.Winforms.UserManager/ListUsersForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Estanoco.MA.Winforms.UserManager
+{
+    partial class ListUsersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(70, 105);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(283, 240);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(58, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(173, 37);
+            label1.TabIndex = 1;
+            label1.Text = "List of Users";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(92, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "NEW";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(251, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "VIEW";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // ListUsersForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources._69237;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(450, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            DoubleBuffered = true;
+            Name = "ListUsersForm";
+            Text = "ListUsersForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Estanoco.MA.Winforms.UserManager/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Estanoco.MA.Winforms.UserManager
+{
+    public class UserRepository
+    {
+        private readonly ISQLiteConnection connection;
+        public UserRepository()
+        {
+            connection = new SQLiteConnection("userdate.db");
+        }
+     
+        public bool Add(User user)
+        {
+            return true;
+        }
+        public List<User> GetAll()
+            {
+            return new List<User>();
+            }
+    public User Get(int id)
+        {
+            return new User("DummyFullname", "DummyUsername", "DummyPassword");
+        }
+
+    }
+}
